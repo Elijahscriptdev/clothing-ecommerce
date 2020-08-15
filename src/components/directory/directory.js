4821f963fd6f2873dec320450a125faf3e3ffcfb
Elijahscriptdev <elijahobominuru@gmail.com>
2020-08-16 00:22:02 +0100

    add styling to menu item component

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -34,6 +34,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
+                  size: 'large',
                   linkUrl: 'shop/womens'
                 },
                 {
@@ -41,6 +42,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
+                  size: 'large',
                   linkUrl: 'shop/mens'
                 }
               ]
@@ -51,8 +53,8 @@ class Directory extends React.Component {
       return(
         <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id}) => (
-                <MenuItem key={id} title={title}/>))
+                this.state.sections.map(({title, imageUrl, id, size}) => (
+                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>))
             }
         </div>
       )
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -5,11 +5,15 @@ import './menu-item.styles.scss'
 * @function MenuItem
 **/
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
   return(
-        <div className='menu-item'>
+        <div className={ `${size} menu-item`}
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        >
           <div className='content'>
-            <div className='title'>{ title }</div>
+            <div className='title'>{ title.toUpperCase() }</div>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
