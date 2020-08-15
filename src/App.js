0dc3669f52ad52220a5e3ce6d4d4bc8eda6e55ef
Elijahscriptdev <elijahobominuru@gmail.com>
2020-08-15 23:40:10 +0100

    set up homepage and directory components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage';
+
 
 function App() {
   return (
     <div>
-      hello world
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './menu-item.styles.scss'
+/**
+* @author
+* @function MenuItem
+**/
+
+const MenuItem = ({ title }) => {
+  return(
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>{ title }</div>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+   )
+
+ }
+
+export default MenuItem
