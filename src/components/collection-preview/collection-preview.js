6a33338efb129a1f42125e67ab8a884306f5a8de
Elijahscriptdev <elijahobominuru@gmail.com>
2020-08-19 01:37:20 +0100

    update collections preview and collection items on shop page

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item';
 
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
@@ -7,10 +8,11 @@ const CollectionPreview = ({ title, items }) => (
         <div className="preview">
             { items
                 .filter((item, idx) => idx <= 4)
-                .map( item => (
-                <div key={ item.id }>
-                    { item.name }
-                </div>
+                .map(({ id, ...otherItemProps })  => (
+                <CollectionItem
+                key={ id }
+                { ...otherItemProps }
+                />
                 ) 
             )}
         </div>
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SHOP_DATA from './shop.data';
 import './shop.styles.scss';
 import CollectionPreview from '../../components/collection-preview/collection-preview';
+// import CollectionItem from '../../components/collection-item/collection-item';
 
 class ShopPage extends React.Component {
     constructor(props){
@@ -18,6 +19,7 @@ class ShopPage extends React.Component {
         <div className="shop-page">
             {
                 collections.map(({ id, ...otherCollectionProps }) => (
+                    // <CollectionItem key={id} { ...otherCollectionProps } />
                     <CollectionPreview key={id} { ...otherCollectionProps }/>
                 ))
             }
