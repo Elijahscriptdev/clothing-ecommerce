27bbb10e9ae25d1c1891ded818bb8637796b03f3
Elijahscriptdev <elijahobominuru@gmail.com>
2020-09-06 01:24:47 +0100

    storing data in the app to login user

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -19,9 +19,22 @@ class App extends React.Component {
 unsubscribeFromAuth = null;
 
 componentDidMount(){
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({ currentUser: user})
-    console.log(user)
+  this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+    if(userAuth){
+      const userRef = await createUserProfileDocument(userAuth);
+      
+      userRef.onSnapshot(snapShot => {
+        this.setState({ 
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        })
+      })
+      
+    }
+    
+    this.setState({ currentUser: userAuth})
   })
 }
 
